36c8e47426e6fa9c9c420e6393e03e9904f93ad7
htung0403 <htung0403@gmail.com>
2023-03-31 16:14:17 +0700

    Initial commit

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace LibraryManagement
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.cbShow = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(235, 122);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(131, 32);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btExit_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(61, 125);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(131, 32);
+            this.btnLogin.TabIndex = 13;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btLogin_Click);
+            // 
+            // cbShow
+            // 
+            this.cbShow.AutoSize = true;
+            this.cbShow.Location = new System.Drawing.Point(144, 96);
+            this.cbShow.Name = "cbShow";
+            this.cbShow.Size = new System.Drawing.Size(114, 20);
+            this.cbShow.TabIndex = 12;
+            this.cbShow.Text = "Hiện mật khẩu";
+            this.cbShow.UseVisualStyleBackColor = true;
+            this.cbShow.CheckedChanged += new System.EventHandler(this.cbShow_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Username";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(134, 68);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(200, 22);
+            this.txtPass.TabIndex = 9;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(134, 40);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(200, 22);
+            this.txtUsername.TabIndex = 8;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 195);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.cbShow);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUsername);
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LoginForm_FormClosing_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.CheckBox cbShow;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtUsername;
+    }
+}
---
+++ LoginForm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace LibraryManagement
+{
+    public partial class LoginForm : Form
+    {
+        SqlConnection conn = new SqlConnection(
+        new SqlConnectionStringBuilder()
+        {
+            DataSource = "hoangtung",
+            InitialCatalog = "LIBRARY",
+            IntegratedSecurity = true,
+            UserID = "TenDangNhap",
+            Password = "MatKhau"
+        }.ConnectionString
+        );
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                e.Cancel = true; // hủy sự kiện FormClosing
+            }
+        }
+
+        private void btExit_Click(object sender, EventArgs e)
+        {
+            DialogResult dg = MessageBox.Show("Bạn có muốn thoát ?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dg == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "DangNhapC#";
+            cmd.Parameters.AddWithValue("@UserName", txtUsername.Text);
+            cmd.Parameters.AddWithValue("@Password", txtPass.Text);
+            cmd.Connection = conn;
+            object kq = cmd.ExecuteScalar();
+            int code = Convert.ToInt32(kq);
+
+            if (code == 1)
+            {
+                MessageBox.Show("Chào mừng đăng nhập", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                Form1 f1 = new Form1();
+                f1.Visible = true;
+                f1.Show();
+                
+            }
+            else if (code == 2)
+            {
+                MessageBox.Show("Tài khoản hoặc mật khẩu không đúng !!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtPass.Text = "";
+                txtUsername.Text = "";
+                txtUsername.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Tài khoản không tồn tại !!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtPass.Text = "";
+                txtUsername.Text = "";
+                txtUsername.Focus();
+            }
+            conn.Close();
+            
+            //conn.Open();
+            //string s = conn.State.ToString();
+            //MessageBox.Show(s, "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void cbShow_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbShow.Checked)
+            {
+                txtPass.PasswordChar = (char)0;
+            }
+            else
+            {
+                txtPass.PasswordChar = '*';
+            }
+        }
+
+        private void LoginForm_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                e.Cancel = true; // hủy sự kiện FormClosing
+            }
+        }
+    }
+}
